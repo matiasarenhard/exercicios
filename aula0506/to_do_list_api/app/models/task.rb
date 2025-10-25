5d3bb844edbc3b108c353b0152620a470c9791c3
matias <matias_arenhard@hotmail.com>
2025-10-25 15:44:04 -0300

    Adicionando status e validações

--- aula0506/to_do_list_api/app/models/task.rb
+++ aula0506/to_do_list_api/app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :title, :description, :status, presence: true
+
+  enum :status, {
+    initial: "initial",
+    in_progress: "in_progress",
+    overdue: "overdue",
+    completed: "completed",
+    cancelled: "cancelled"
+  }, default: :initial
+
 end
